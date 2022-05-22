5e81ca8c295ec08faf64963b1b8c3901af32ff79
VitalijusSeskovas <vitadis5@yahoo.co.uk>
2022-05-22 09:54:21 +0000

    Add types of functions,quastion and answers sections to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Creating all required elements for reacting buttons, Welcome and Question sections
+let startButton = document.getElementById('start-btn');
+let nextButton = document.getElementById('next-btn');
+let questionBoxElement = document.getElementById('question-box');
+let questionElement = document.getElementById('question');
+let answerButtonsElement = document.getElementById('answer-buttons');
+
+// When Start Button click
+function startGame ()
+
+// When Next Button click to set next questions
+function setNextQuestion()
+
+function showQuestion()
+// Will rest question section bact to defaul state
+function resetState()
+
+// When user selet the answer
+function selectAnswer()
+
+// Quastions and Answers
+let questions

939a6744b64839e67cf179598fb6618cc5531b8c
VitalijusSeskovas <vitadis5@yahoo.co.uk>
2022-05-22 10:00:22 +0000

    Add quastions and answer options, buttons on click event to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,141 @@ let questionBoxElement = document.getElementById('question-box');
 let questionElement = document.getElementById('question');
 let answerButtonsElement = document.getElementById('answer-buttons');
 
+// Element for making quastions and answers apear random
+let shuffleQuestions, currentquestionIndex;
+
 // When Start Button click
-function startGame ()
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentquestionIndex++;
+    setNextQuestion();
+});
 
-// When Next Button click to set next questions
-function setNextQuestion()
+/*
+The code below will hide
+Start Button and will make
+questions apear not in
+order but random 'shuffleQuestions':
+*/
+function startGame () {
+    startButton.classList.add('hide');
+    shuffleQuestions = questions.sort(() => Math.random() - .5);
+    currentquestionIndex = 0;
+    questionBoxElement.classList.remove('hide');
+    setNextQuestion();
 
-function showQuestion()
-// Will rest question section bact to defaul state
-function resetState()
+// When Next Button click
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffleQuestions[currentquestionIndex]);
+}
 
+// After Next Buutn click will show next quastions and answear options
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
 // When user selet the answer
 function selectAnswer()
 
 // Quastions and Answers
-let questions
+let questions = [
+    {
+        question: 'What is a female who practices yoga called????',
+        answers: [
+          { text: 'A. Sutra', correct: false },
+          { text: 'B. Yogi', correct: false },
+          { text:'C. Yogini', correct: true },
+          { text: 'D. Buddhist', correct: false }
+        ]
+    },
+    {
+        question: "What is traditionally chanted at the beginning and end of a yoga session?",
+        answers: [
+            { text: 'A. Hatha', correct: false },
+          { text: 'B. Om', correct: true },
+          { text:'C. Asana', correct: false },
+          { text: 'D. Samadhi', correct: false }
+        ]
+    },
+    {
+        question: "Where did yoga originate?",
+        answers: [
+            { text: 'A. Bharta', correct: false },
+            { text: 'B. Thailand', correct: false },
+            { text:'C. Nepal', correct: false },
+            { text: 'D. India', correct: true }
+          ]
+    },
+    {
+        question: "What does the word yoga mean?",
+        answers: [
+            { text: 'A. Union', correct: true },
+            { text: 'B. Peace', correct: false },
+            { text:'C. Relaxation', correct: false },
+            { text: 'D. Soul', correct: false }
+          ]
+    },
+    {
+        question: "In what yoga position do you stand, place a hand on the floor and stretch your arms?",
+        answers: [
+            { text: 'A. Cat', correct: false },
+            { text: 'B. Warrior', correct: false },
+            { text:'C. Triangle', correct: true },
+            { text: 'D. Cobra', correct: false }
+          ]
+    },
+    {
+        question: "What Yogic method did Indra Devi advocate above all other techniques?",
+        answers: [
+            { text: 'A. Plough Pose', correct: false },
+            { text: 'B. Vegan Diet', correct: false },
+            { text:'C. Headstand', correct: false },
+            { text: 'D. Deep Rhythmic Breathing', correct: true }
+          ]
+    },
+    {
+        question: "What teacher inspired the creation of the Sivananda Yoga Vedanta Centers?",
+        answers: [
+            { text: 'A. Swami Vishnu-devanada', correct: false },
+            { text: 'B. Swami Sivananda', correct: true },
+            { text:'C. Swami Krishnananda', correct: false },
+            { text: 'D. Swami Vivekanada', correct: false }
+          ]
+    },
+    {
+        question: "What is the name for the yoga of meditation?",
+        answers: [
+            { text: 'A. Jnana Yoga', correct: false },
+            { text: 'B. Bhakti Yoga', correct: false },
+            { text:'C. Raja Yoga', correct: true },
+            { text: 'D. Karma Yoga', correct: false }
+          ]
+    },
+    {
+        question: "What does Raja Yoga advocate?",
+        answers: [
+            { text: 'A. Mental And Physical Control ', correct: true },
+            { text: 'B. Purity Of Heart', correct: false },
+            { text:'C. Divine Love', correct: false },
+            { text: 'D. Knowledge', correct: false }
+          ]
+    },
+    {
+        question: "What is the name for a sequence of yoga positions?",
+        answers: [
+            { text: 'A. Tadasana', correct: false },
+            { text: 'B. Poses', correct: false },
+            { text:'C. Bhujangasana', correct: false },
+            { text: 'D. Vinyasa', correct: true }
+          ]
+    },
+];
